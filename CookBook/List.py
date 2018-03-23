a7cbb3d73d529b84a3a4b18f5ee7ea0c43d8544d
vonsago <f811194414@gmail.com>
2018-03-23 11:39:06 +0800

    update List

--- CookBook/List.py
+++ CookBook/List.py
@@ -8,7 +8,8 @@
 '''
 from collections import Counter
 from operator import itemgetter
-'''note 1 列表元素去重
+'''
+---note 1 列表元素去重
 >>> a = [ {'x':1, 'y':2}, {'x':1, 'y':3}, {'x':1, 'y':2}, {'x':2, 'y':4}]
 >>> list(dedupe(a, key=lambda d: (d['x'],d['y'])))
 [{'x': 1, 'y': 2}, {'x': 1, 'y': 3}, {'x': 2, 'y': 4}]
@@ -16,8 +17,8 @@ from operator import itemgetter
 [{'x': 1, 'y': 2}, {'x': 2, 'y': 4}]
 >>>
 key 函数参数模仿了 sorted() , min() 和 max() 等内置函数的相似功能
-'''
-'''note 2 slice
+
+---note 2 slice
 内置函数，切片对象
 >>> a = slice(5, 50, 2)
 >>> s = 'HelloWorld'
@@ -30,6 +31,18 @@ W
 r
 d
 通过调用切片的 indices(size) 方法将它映射到一个确定大小的序 列上，这个方法返回一个三元组 (start, stop, step) ，所有值都会被合适的缩小以 满足边界限制，从而使用的时候避免出现 IndexError 异常
+
+---note 3列表推导与过滤
+列表推导的一个潜在缺陷就是如果输入非常大的时候会产生一个非常大的结 果集，占用大量内存
+可以使用生成器表达式迭代产生 过滤的元素
+>>> pos = (n for n in mylist if n > 0)
+>>> pos
+<generator object <genexpr> at 0x1006a0eb0> 
+>>> for x in pos:
+... print(x)
+
+filter()
+
 '''
 
 def dedupe(items, key=None):
@@ -72,4 +85,12 @@ if __name__ == '__main__':
 
     '''
     print('---note 3---')
-
+    values = ['1', '2', '-3', '-', '4', 'N/A', '5'] 
+    def is_int(val):
+        try:
+            _ = int(val)
+            return True
+        except ValueError:
+            return False
+    ivals = list(filter(is_int, values)) 
+    print(ivals)
