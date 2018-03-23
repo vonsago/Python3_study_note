e0fb6acb94f1c2f064e12eccf58feacc70774b2d
vonsago <f811194414@gmail.com>
2018-03-23 16:03:15 +0800

    update Collection

--- CookBook/Collection.py
+++ CookBook/Collection.py
@@ -6,8 +6,8 @@
 > Mail: f811194414@gmail.com
 > Created Time: 四  3/22 14:22:27 2018
 '''
-from collections import OrderedDict, defaultdict \
-        ,namedtuple
+from collections import OrderedDict, defaultdict , namedtuple \
+        , ChainMap
 '''
 ---note 1 OrderedDict
 创建一个字典，并且在迭代或序列化这个字典的时候能够控制元素的顺序
@@ -17,8 +17,18 @@ from collections import OrderedDict, defaultdict \
 
 ---note 3 namedtuple
 
+---note 4 ChainMap
+一个 ChainMap 接受多个字典并将它们在逻辑上变为一个字典。然后，这些字典并 不是真的合并在一起了，ChainMap 类只是在内部创建了一个容纳这些字典的列表并重 新定义了一些常见的字典操作来遍历这个列表。大部分字典操作都是可以正常使用的
+>>> len(c)
+3
+>>> list(c.keys()) ['x', 'y', 'z']
+>>> list(c.values()) [1, 2, 3]
+如果出现重复键，那么第一次出现的映射值会被返回
+对于字典的更新或删除操作总是影响的是列表中第一个字典
+ChainMap 使用原来的字典，它自己不创建新的字典(意味着更新原字典会影响ChainMap的结果)
 
-
+update()
+a.update(b)用于合并两个字点
 '''
 
 def compute_cost(records):
@@ -50,3 +60,9 @@ if __name__ == '__main__':
     s = Stock('ACME', 100, 123.45)
     s = s._replace(**{'shares':75}) # or 'shares'=75
     print(s)
+
+    print('---note 3---')
+    a = {'x': 1, 'z': 3 }
+    b = {'y': 2, 'z': 4 }
+    c = ChainMap(a,b)
+    print(c)
--- CookBook/Dict.py
+++ CookBook/Dict.py
@@ -17,6 +17,29 @@ print(max(prices_and_names)) # ValueError: max() arg is an empty sequence
 ---note 2 两个字典中寻找相同点
 items()
 以列表返回可遍历的(键, 值) 元组数组
+
+
+
+---note 3 * **
+def test_args_kwargs(arg1, arg2, arg3):
+    print("arg1:", arg1)
+    print("arg2:", arg2)
+    print("arg3:", arg3)
+
+# first with *args
+>>> args = ("two", 3, 5)
+>>> test_args_kwargs(*args)
+arg1: two
+arg2: 3
+arg3: 5
+
+# now with **kwargs:
+>>> kwargs = {"arg3": 3, "arg2": "two", "arg1": 5}
+>>> test_args_kwargs(**kwargs)
+arg1: 5
+arg2: two
+arg3: 3
+
 '''
 
 if __name__ == '__main__':
