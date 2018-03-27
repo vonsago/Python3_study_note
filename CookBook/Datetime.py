e663eb1ff4cc9d016627669402d60501502427a2
vonsago <f811194414@gmail.com>
2018-03-27 17:44:20 +0800

    update

--- CookBook/Datetime.py
+++ CookBook/Datetime.py
@@ -8,12 +8,28 @@
 '''
 
 from datetime import timedelta, datetime
-
+from pytz import timezone
 '''
 ---note 1---datetime
 
 ---note 2---dateutil
+
+---note 3---from pytz import timezone
+处理本地化日期的通常的策略先将所有日 期转换为 UTC 时间，并用它来执行所有的中间存储和操作
+>>> print(loc_d)
+2013-03-10 01:45:00-06:00
+>>> utc_d = loc_d.astimezone(pytz.utc) 
+>>> print(utc_d)
+2013-03-10 07:45:00+00:00
+>>> later_utc = utc_d + timedelta(minutes=30) 
+>>> print(later_utc.astimezone(central)) 2013-03-10 03:15:00-05:00
+>>> pytz.country_timezones['IN'] 
+['Asia/Kolkata']
+
+hhhhhh:
+注:当你阅读到这里的时候，有可能 pytz 模块已经不再建议使用了
 '''
+
 
 if __name__ == '__main__':
     print('为了表示一 个时间段，可以创建一个 timedelta 实例')
@@ -30,4 +46,17 @@ if __name__ == '__main__':
     print(a - datetime(2018,3,22))
     print(datetime.today())
     print('要注意的是 datetime 会自动处理闰年')
+    text = '2012-09-20'
+    print("注意datetime.strptime 性能较差")
+    y = datetime.strptime(text, '%Y-%m-%d')
+    print(y)
+    print('几乎所有涉及到时区的问题，你都应该使用 pytz 模块')
+    d = datetime(2012, 12, 21, 9, 30, 0)
+    print(d)
+    central = timezone('US/Central')
+    loc_d = central.localize(d)
+    print('一旦日期被本地化了，它就可以转换为其他时区的时间了')
+    print(loc_d)
+    bang_d = loc_d.astimezone(timezone('Asia/Kolkata'))
+    print(bang_d)
     
