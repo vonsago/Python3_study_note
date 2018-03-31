33d19ea2ec42e917d2fb5235764a9142c733fd14
vonsago <f811194414@gmail.com>
2018-03-31 11:39:03 +0800

    partial

--- CookBook/Func.py
+++ CookBook/Func.py
@@ -13,16 +13,23 @@ from functools import partial
 functools.partial 用来创建一个每次被调用时从文件中读取固定数
 目字节的可调用对象。标记值 b'' 就是当到达文件结尾时的返回值
 
-在一个固定长度记录或者数据块的集合上迭代，而不是在一个文件中一行一 行的迭代
+在一个固定长度记录或者数据块的集合上迭代，而不是在一个文件中一行一行的迭代
+
+也可使用 partial() 函数来固定某些参数值
+
 '''
 
 
+def test(a, b, c, d):
+    return a, b, c, d
 
 
 if __name__ == '__main__':
-    RECORD_SIZE = 32
-    with open('somefile.data', 'rb') as f:
-        records = iter(partial(f.read, RECORD_SIZE), b'') 
-        for r in records:
-            print(r)
+    #RECORD_SIZE = 32
+    #with open('somefile.data', 'rb') as f:
+    #    records = iter(partial(f.read, RECORD_SIZE), b'') 
+    #    for r in records:
+    #        print(r)
 
+    s = partial(test, 1,d=4)
+    print(s(2,3))
