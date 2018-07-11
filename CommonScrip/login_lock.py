ddc8ba16660f68816ebd03fecb0556a61759f90a
vonsago <f811194414@gmail.com>
2018-07-11 20:17:01 +0800

    finish conform str

--- CommonScrip/login_lock.py
+++ CommonScrip/login_lock.py
@@ -20,17 +20,37 @@ def conform_password_level(password, password_level):
     if len(password) < password_level:
         return False
     if password_level == 8:
-        if re.search('[0-9]', password)==None or re.search('[a-z]', password)==None or re.search(['A-Z', password])==None:
+        if re.search('[0-9]', password)==None or re.search('[a-z]', password)==None or re.search('[A-Z]', password)==None:
             return False
     if password_level == 10:
         if re.search('\W', password)==None:
             return False
     return True
 
-def judge_locking():
-
+def update_password():
     pass
+def login(name, password, last_login_time, last_fail_times, max_try_times):
+    pass
+
+def judge_locking(name, last_login_time, last_fail_times, max_try_times):
+    if last_fail_times==0 or time.time()-last_login_time > 86400:
+        last_login_time = time.time()
+        last_fail_times = 1
+    else:
+        last_fail_times += 1
+    if last_fail_times > max_try_times:
+        return False
+    #update database table[name]
+    return login()
 
 
 if __name__ == '__main__':
-    pass
+    s1 = '123'
+    s2 = 'abc'
+    s3 = s1+s2
+    s4 = s3+ "%*_"
+    
+    print(conform_password_level(s1,0))
+    print(conform_password_level(s1+s1,1))
+    print(conform_password_level(s2+s3+"AWEF",2))
+    print(conform_password_level(s4+"DSFGJAS",3))
