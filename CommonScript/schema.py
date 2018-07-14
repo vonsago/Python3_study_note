1754134610655a2fbaad4e29a9b9dfc4a99029e0
vonsago <f811194414@gmail.com>
2018-07-14 15:14:13 +0800

    marshmallow

---
+++ CommonScript/schema.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding=utf-8
+'''
+> File Name: schema.py
+> Author: vassago
+> Mail: f811194414@gmail.com
+> Created Time: 六  7/14 15:13:17 2018
+'''
+from datetime import date
+from marshmallow import Schema, fields, pprint
+
+
+if __name__ == "__main__":
+    pass
