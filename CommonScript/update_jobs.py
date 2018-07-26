51f82cf9daa06323db953da14a19f20919886ddc
vonsago <f811194414@gmail.com>
2018-07-26 12:02:29 +0800

    done

--- CommonScript/update_jobs.py
+++ CommonScript/update_jobs.py
@@ -12,14 +12,13 @@ import sys
 import time
 import json
 
-IP = "192.168.101.26"
 URL = "http://{ip}/api/v1/namespaces/{namespace}/pods"
-COMMON = "curl {url}"
+COMMON = "curl -s -u {passkey} {url}"
 # git rev-parse HEAD
 
 def list_pod_names():
     pods_url = URL.format(ip=IP, namespace="default")
-    pods = os.popen(COMMON.format(url=pods_url)).read()
+    pods = os.popen(COMMON.format(passkey=passkey,url=pods_url)).read()
     lis = json.loads(pods)['items']
     pod_names = [i["metadata"]["selfLink"] for i in lis]
     return pod_names
@@ -27,7 +26,7 @@ def list_pod_names():
 def get_pod_imageVersion(pod_name):
     #print("pod name:",pod_name)
     url = "http://"+IP+pod_name
-    version = os.popen(COMMON.format(url=url)).read()
+    version = os.popen(COMMON.format(passkey=passkey,url=url)).read()
     version=json.loads(version)
     container = version["status"]["containerStatuses"][0]
     image = container['image'].split(':')[-1]
@@ -43,24 +42,26 @@ def check_name_equal(n1,n2):
 def check_updated(app_name, image):
     pnames = list_pod_names()
     for p in pnames:
+        print(p, app_name)
         if check_name_equal(p,app_name):
             im = get_pod_imageVersion(p)
             if im == image:
                 upup.append(p.split('/')[-1])
 
 if __name__ == "__main__":
-    # python update_jobs.py dsp-dev develop-31c4e8d api
     app_name = sys.argv[1]
     image = sys.argv[2]
     ty = sys.argv[3]
-    for i in range(10):
+    IP = sys.argv[4]
+    passkey = sys.argv[5]
+    for i in range(100):
         upup = []
         check_updated(app_name, image)
         print(upup)
         if ty == 'api' and len(upup) == 4:
-            exit(1)
+            exit(0)
         elif ty == 'ui' and len(upup) == 1:
-            exit(1)
+            exit(0)
         time.sleep(10)
 
-    exit(0)
+    exit(1)
