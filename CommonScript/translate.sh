15ffe5078223543009b934a50df6e203ac071289
vonsago <f811194414@gmail.com>
2018-08-03 01:51:38 +0800

    translate

--- CommonScript/translate.sh
+++ CommonScript/translate.sh
@@ -7,3 +7,12 @@
 echo "翻译完成[y/n]"
 read nu
 if [ $nu = "y" ]; then pybabel compile -d static/i18n/; else echo "没有编译po文件";fi
+
+
+#pybabel extract -F static/i18n/babel.cfg -k lazy_gettext -o static/i18n/messages.pot .
+#
+#pybabel update -i static/i18n/messages.pot -d static/i18n/
+#
+#echo "翻译完成[y/n]"
+#read nu
+#if [ $nu = "y" ]; then pybabel compile -d static/i18n/; rm -f static/i18nn/messages.pot; else echo "手动执行: pybabel update -i static/i18n/messages.pot -d static/i18n/";fi
