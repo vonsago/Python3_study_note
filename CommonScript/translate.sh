9267be1fc67df2f1e8b00d7999ce0905815f12f0
vonsago <f811194414@gmail.com>
2018-08-02 21:21:19 +0800

    sh translate

---
+++ CommonScript/translate.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#pybabel extract -F static/i18n/babel.cfg -k lazy_gettext -o static/i18n/messages.pot .
+
+#pybabel update -i static/i18n/messages.pot -d static/i18n/
+
+echo "翻译完成[y/n]"
+read nu
+if [ $nu = "y" ]; then pybabel compile -d static/i18n/; else echo "没有编译po文件";fi
