cca3a6f1d8b21960d771549c43de21635700b325
vonsago <f811194414@gmail.com>
2018-08-07 11:25:30 +0800

    8.24

--- CookBook/Clas.py
+++ CookBook/Clas.py
@@ -146,7 +146,7 @@ class Stock:
         self.price = price
         print(self.__dict__)
 
-#---5
+#---5 8.10
 class lazyproperty:
     def __init__(self, func):
         self.func = func
@@ -220,6 +220,36 @@ class Spam:
         self.x = x
     def bar(self, y):
         print('Spam.bar:', self.x, y)
+
+#---
+#* 8.24
+from functools import total_ordering
+
+class Room:
+    def __init__(self, name, length, width):
+        self.name = name
+        self.length = length
+        self.width = width
+        self.square_feet = self.length * self.width
+@total_ordering
+class House:
+    def __init__(self, name, style):
+        self.name = name
+        self.style = style
+        self.rooms = list()
+    @property
+    def living_space_footage(self):
+        return sum(r.square_feet for r in self.rooms)
+    def add_room(self, room):
+        self.rooms.append(room)
+    def __str__(self):
+        return '{}: {} square foot {}'.format(self.name,
+                    self.living_space_footage,
+                    self.style)
+    def __eq__(self, other):
+        return self.living_space_footage == other.living_space_footage
+    def __lt__(self, other): 
+        return self.living_space_footage < other.living_space_footage
 
 
 if  __name__ == '__main__':
