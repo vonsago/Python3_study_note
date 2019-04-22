22532b73e807015645c313cbf8ea0b4673dd5d2c
vonsago <f811194414@gmail.com>
2019-04-22 21:05:12 +0800

    add eg sort

--- CommonScript/sort_eg.py
+++ CommonScript/sort_eg.py
@@ -15,3 +15,11 @@ mydict={ 'Li': ['M',7],
 'Zhe': ['H',7] }
 sorted(mydict.iteritems(), key=lambda (k,v): operator.itemgetter(1)(v))”
 
+
+gameresult = [{ "name":"Bob", "wins":10, "losses":3, "rating":75.00 },
+...             { "name":"David", "wins":3, "losses":5, "rating":57.00 },
+...             { "name":"Carol", "wins":4, "losses":5, "rating":57.00 },
+...             { "name":"Patty", "wins":9, "losses":3, "rating": 71.48 }]
+>>> from operator import itemgetter
+>>> sorted(gameresult , key=operator.itemgetter("rating","name"))”
+
